0e141108da8da5929013bff5f1d94391b0d70f06
imxzjv <imxzjv@8b886bb1-50ed-47f6-93b4-c619e4d10bba>
2016-05-22 03:19:23 +0000

    * Resetting Production Branch

--- branches/Production/Framework/Objects/Memory/Sno/SnoTableEntry.cs
+++
@@ -1,8 +0,0 @@
-namespace Trinity.Framework.Objects.Memory.Sno
-{
-    public class SnoTableEntry : MemoryWrapper
-    {
-        public SnoHeader Header => ReadObject<SnoHeader>(0x00);
-
-    }
-}
--- branches/Production/Framework/Objects/Memory/Symbols/Types/DensityType.cs
+++
@@ -1,8 +0,0 @@
-namespace Trinity.Framework.Objects.Memory.Misc
-{
-    public enum DensityType // 2.4.0.36090 @26702088 index:31
-    {
-        Countper100Sqft = 0,
-        Exactly = 1,
-    }
-}
