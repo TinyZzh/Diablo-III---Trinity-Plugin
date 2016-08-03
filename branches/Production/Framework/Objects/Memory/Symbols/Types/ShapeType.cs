cd90cbd82157a3a0691dbbb57fd08ee2f2e6d0de
imxzjv <imxzjv@8b886bb1-50ed-47f6-93b4-c619e4d10bba>
2016-08-03 00:22:48 +0000

    just replacing as merge is a disaster.

--- branches/Production/Framework/Objects/Memory/Symbols/Types/ShapeType.cs
+++
@@ -1,9 +0,0 @@
-namespace Trinity.Framework.Objects.Memory.Misc
-{
-    public enum ShapeType // 2.4.0.36090 @26702256 index:35
-    {
-        Line = 0,
-        Cross = 1,
-        Circle = 2,
-    }
-}
